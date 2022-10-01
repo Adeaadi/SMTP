706d8193c4b6bef76c758665e0b52eb8e440b729
Adeaadi <113269585+Adeaadi@users.noreply.github.com>
2022-10-01 13:36:44 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,7 +2,7 @@ from socket import *
 
 
 def smtp_client(port=1025, mailserver='127.0.0.1'):
-    msg = "\r\n This class gives me a headache"
+    msg = "\r\nThis class gives me a headache"
     endmsg = "\r\n.\r\n"
 
     # Choose a mail server (e.g. Google mail server) if you want to verify the script beyond GradeScope
@@ -20,17 +20,11 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     # Fill in end
 
     recv = clientSocket.recv(1024).decode()
-    # print(recv) #You can use these print statement to validate return codes from the server.
-    # if recv[:3] != '220':
-    #    print('220 reply not received from server.')
 
     # Send HELO command and print server response.
     heloCommand = 'HELO Alice\r\n'
     clientSocket.send(heloCommand.encode())
     recv1 = clientSocket.recv(1024).decode()
-    # print(recv1)
-    # if recv1[:3] != '250':
-    #    print('250 reply not received from server.')
 
     # Send MAIL FROM command and handle server response.
     # Fill in start
@@ -87,6 +81,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     print(recv6)
     if recv6[:3] != '221':
         print('221 reply not received from server.')
+    clientSocket.close()
 # Fill in end
 
 
